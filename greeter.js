c7267af32ec28a673c7bce7f2248a6e7993e633f
NGNikolay <georgiev.nikolaj@gmail.com>
2024-04-27 11:27:58 +0300

    playing with errors

--- greeter.js
+++ greeter.js
@@ -1,5 +1,5 @@
 function greeter(person) {
     return 'Hello ' + person;
 }
-var user = 'Jane User';
+var user = [0, 1, 2];
 document.body.textContent = greeter(user);
--- greeter.ts
+++ greeter.ts
@@ -1,6 +1,6 @@
-function greeter(person) {
+function greeter(person: string) {
   return 'Hello ' + person;
 }
 
-let user = 'Jane User';
+let user = [0, 1, 2];
 document.body.textContent = greeter(user);
