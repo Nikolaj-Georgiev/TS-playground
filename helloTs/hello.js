7962611e969e6b5ea2b5d588107d1fa2750f5fd0
NGNikolay <georgiev.nikolaj@gmail.com>
2024-04-30 09:28:56 +0300

    trying different types

--- helloTs/hello.js
+++ helloTs/hello.js
@@ -1,3 +1,4 @@
+'use strict';
 // This is an industrial-grade general-purpose greeter function:
 function greet(person, date) {
     console.log("Hello ".concat(person, ", today is ").concat(date.toDateString()));
--- helloTs/hello.ts
+++ helloTs/hello.ts
@@ -1,3 +1,4 @@
+'use strict';
 // This is an industrial-grade general-purpose greeter function:
 function greet(person: string, date: Date) {
   console.log(`Hello ${person}, today is ${date.toDateString()}`);
