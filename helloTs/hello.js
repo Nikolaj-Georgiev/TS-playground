f7a87e9d425bc42d184d6d1181e9fa44137de60c
NGNikolay <georgiev.nikolaj@gmail.com>
2024-04-30 09:17:11 +0300

    playing with errors

--- helloTs/hello.js
+++ helloTs/hello.js
@@ -1,5 +1,5 @@
 // This is an industrial-grade general-purpose greeter function:
 function greet(person, date) {
-    console.log("Hello ".concat(person, ", today is ").concat(date));
+    console.log("Hello ".concat(person, ", today is ").concat(date.toDateString()));
 }
-greet('Brandan');
+greet('Maddison', Date());
--- helloTs/hello.ts
+++ helloTs/hello.ts
@@ -3,4 +3,4 @@ function greet(person: string, date: Date) {
   console.log(`Hello ${person}, today is ${date.toDateString()}`);
 }
 
-greet('Brandanа');
+greet('Madison', Date());
