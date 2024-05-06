5d5c7cb1724a717b35f35025b425dceeaefdf4a4
NGNikolay <georgiev.nikolaj@gmail.com>
2024-05-06 16:24:23 +0300

    array type

--- helloTs/hello.js
+++ helloTs/hello.js
@@ -28,3 +28,25 @@ function printId(id) {
 }
 printId('Pesho');
 printId('42');
+// printId({ pesho: 42 }); //hello.ts:39:9 - error TS2345: Argument of type '{ pesho: number; }' is not assignable to parameter of type 'string | number'.
+function printId1(id) {
+    // console.log(id.toUpperCase());//Property 'toUpperCase' does not exist on type 'string | number'.Property 'toUpperCase' does not exist on type 'number'.ts(2339)
+    if (typeof id === 'string') {
+        console.log(id.toUpperCase());
+    }
+    else {
+        console.log(id);
+    }
+}
+function welcomePeople(x) {
+    if (Array.isArray(x)) {
+        console.log('Hello ' + x.join(' and '));
+        // Here: 'x' is 'string[]'
+    }
+    else {
+        console.log('Welcome lone traveler ' + x);
+        // Here: sx is a string
+    }
+}
+welcomePeople(['Pesho', 'Gosho', 'Atanas', 'Bai Hui']);
+welcomePeople('Suzana');
--- helloTs/hello.ts
+++ helloTs/hello.ts
@@ -38,3 +38,26 @@ function printId(id: number | string) {
 printId('Pesho');
 printId('42');
 // printId({ pesho: 42 }); //hello.ts:39:9 - error TS2345: Argument of type '{ pesho: number; }' is not assignable to parameter of type 'string | number'.
+
+function printId1(id: number | string) {
+  // console.log(id.toUpperCase());//Property 'toUpperCase' does not exist on type 'string | number'.Property 'toUpperCase' does not exist on type 'number'.ts(2339)
+
+  if (typeof id === 'string') {
+    console.log(id.toUpperCase());
+  } else {
+    console.log(id);
+  }
+}
+
+function welcomePeople(x: string[] | string) {
+  if (Array.isArray(x)) {
+    console.log('Hello ' + x.join(' and '));
+    // Here: 'x' is 'string[]'
+  } else {
+    console.log('Welcome lone traveler ' + x);
+    // Here: sx is a string
+  }
+}
+
+welcomePeople(['Pesho', 'Gosho', 'Atanas', 'Bai Hui']);
+welcomePeople('Suzana');
