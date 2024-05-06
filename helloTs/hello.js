2f72970189ff1f81a7d2655dd05ac31f89ae5489
NGNikolay <georgiev.nikolaj@gmail.com>
2024-05-06 10:28:23 +0300

    object types

--- helloTs/hello.js
+++ helloTs/hello.js
@@ -1,6 +1,12 @@
 'use strict';
 // This is an industrial-grade general-purpose greeter function:
 function greet(person, date) {
-    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+    console.log("Hello ".concat(person, ", today is ").concat(date.toDateString(), "!"));
 }
 greet('Madison', new Date());
+// The parameter's type annotation is an object type
+function printCoord(pt) {
+    console.log("The coordinate's x value is " + pt.x);
+    console.log("The coordinate's y value is " + pt.y);
+}
+printCoord({ x: 3, y: 7 });
