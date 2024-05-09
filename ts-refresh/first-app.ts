79839431cb3fd7d525fc1b497765cc72d5c19124
NGNikolay <georgiev.nikolaj@gmail.com>
2024-05-09 23:17:00 +0300

    demo

--- ts-refresh/first-app.ts
+++ ts-refresh/first-app.ts
@@ -1,5 +1,5 @@
 // let userName = 'Niko';
-let userName: string = 'Niko';// no need to explicitly describe the type in this case it will be inferred by TS. You can use the declaration from the upper sentence
+let userName: string = 'Niko'; // no need to explicitly describe the type in this case it will be inferred by TS. You can use the declaration from the upper sentence
 // userName = 34;
 userName = 'Niko';
 
@@ -12,6 +12,24 @@ let isValid = true;
 // string, number, boolean
 
 // union type
-let  userId: number | string = 'abc1';
+let userId: number | string = 'abc1';
 userId = 123;
 
+// Object types
+// let user: object;
+
+let user: {
+  name: string;
+  age: number;
+  isAdmin: boolean;
+  id: string | number;
+};
+
+user = {
+  name: 'Niko',
+  age: 43,
+  isAdmin: true,
+  id: 'abc', //123
+};
+
+// user = {};
