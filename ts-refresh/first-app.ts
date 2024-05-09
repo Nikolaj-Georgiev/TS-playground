2d331aeb438d2cd56d230f1cbdfd6d80ac5ac409
Nikolay Georgiev <georgiev.nikolaj@gmail.com>
2024-05-09 16:11:56 +0300

    demo

--- ts-refresh/first-app.ts
+++ ts-refresh/first-app.ts
@@ -1,6 +1,12 @@
 let userName = 'Niko';
 // let userName: string;
-
-
 // userName = 34;
 userName = 'Niko';
+
+// let userAge:number = 34;
+let userAge = 34;
+
+// let isValid:boolean = true;
+let isValid = true;
+
+// string, number, boolean
