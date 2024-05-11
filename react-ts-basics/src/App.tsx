8f734ec52ddf13b76f77f69471eca6569228fe9b
NGNikolay <georgiev.nikolaj@gmail.com>
2024-05-11 22:38:42 +0300

    using state with TS

--- react-ts-basics/src/App.tsx
+++ react-ts-basics/src/App.tsx
@@ -1,22 +1,51 @@
+import { useState } from 'react';
+
 // import User from './components/Users.tsx';
 import CourseGoal from './components/CourseGoal.tsx';
 import goalsImg from './assets/goals.jpg';
 import Header from './components/Header.tsx';
 
+type CourseGoals = {
+  title: string;
+  description: string;
+  id: number;
+};
+
 export default function App() {
+  // const [goals, setGoals] = useState<Array<CourseGoals>>([]); // alternative to the one below
+  const [goals, setGoals] = useState<CourseGoals[]>([]);
+
   // const users = [{ name: 'John' }, { name: 'Mary' }, { name: 'Luke' }];
+  function handleAddGoal() {
+    setGoals((prevGoals) => {
+      const newGoal: CourseGoals = {
+        id: Math.random(),
+        title: 'Learn React + TS',
+        description: 'Learn it in depth!',
+      };
+      return [...prevGoals, newGoal];
+    });
+  }
   return (
     <main>
       <Header image={{ src: goalsImg, alt: 'A list of Goals' }}>
         <h1>Your Course Goals</h1>
       </Header>
-      <CourseGoal title='Learn React + TS'>
-        <p>Learn it from the ground up</p>
-      </CourseGoal>
+      <button onClick={handleAddGoal}>Add Goal</button>
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal title={goal.title}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+
       {/* <ul>
         {users.map((user, index) => (
           <User
-            key={index}
+          key={index}
             name={user.name}
           />
         ))}
--- react-ts-basics/src/components/Header.tsx
+++ react-ts-basics/src/components/Header.tsx
@@ -1,4 +1,12 @@
 import { type PropsWithChildren } from 'react';
+// import { type ReactNode } from 'react';
+// type HeaderProps = {
+//   children: ReactNode;
+//   image: {
+//     src: string;
+//     alt: string;
+//   };
+// };
 
 type HeaderProps = PropsWithChildren<{ image: { src: string; alt: string } }>;
 
